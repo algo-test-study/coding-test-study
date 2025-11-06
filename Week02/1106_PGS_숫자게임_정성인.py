fb635c765ad6f93d61b4ee978fad7e1634a836a5
jin800812 <77664758+jin800812@users.noreply.github.com>
2025-11-06 22:04:58 +0900

    add: 숫자 게임

---
+++ Week02/1106_PGS_숫자게임_정성인.py
@@ -0,0 +1,14 @@
+def solution(A, B):
+    answer = 0
+    B.sort()
+    A.sort()
+    i, j = 0, 0
+    
+    while i < len(A) and j < len(B):
+        if A[i] < B[j]:
+            answer += 1
+            i += 1
+            j += 1
+        else:
+            j += 1
+    return answer
