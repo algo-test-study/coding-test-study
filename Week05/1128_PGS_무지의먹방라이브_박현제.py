aa242efea2e0e9fc1bc37af2f63c106d3761f264
HyunJe Park <sorsor999@naver.com>
2025-11-28 23:54:48 +0900

    add: 무지의 먹방 라이브

---
+++ Week05/1128_PGS_무지의먹방라이브_박현제.py
@@ -0,0 +1,40 @@
+"""
+답 참고함
+
+알고리즘
+- 완전 탐색 X: 2*10^5*10^8
+- 한번에 한 턴 보기:2 *10^13 / 2*10^5 = 10^8
+- pq에 전체 음식 넣기
+- 작은 음식부터 poll 
+시간복잡도
+- O(10^8)
+"""
+import heapq
+def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+    
+    pq = []
+    
+    for i, v in enumerate(food_times):
+        heapq.heappush(pq, (v, i+1))
+    
+    total = 0  # 지금까지 사용된 누적 시간
+    prev = 0   # 이전 레벨 최소 시간
+    cnt = len(food_times)
+
+    while pq:
+        min_time = pq[0][0]
+        spend = (min_time - prev) * cnt
+
+        if total + spend > k:
+            break
+
+        heapq.heappop(pq)
+        total += spend
+        prev = min_time
+        cnt -= 1
+    
+    pq.sort(key=lambda x: x[1])
+    ans = pq[(k - total) % cnt][1]
+    return ans
