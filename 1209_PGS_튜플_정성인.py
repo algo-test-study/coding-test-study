2a7554c1eaf8b450a4a481acb82465b91527f7c4
jin800812 <77664758+jin800812@users.noreply.github.com>
2025-12-12 23:50:49 +0900

    delete: 잘못된 추가된 파일 삭제

--- 1209_PGS_튜플_정성인.py
+++
@@ -1,8 +0,0 @@
-def solution(s):
-    answer = []
-    _list = sorted(list(s[2:-2].split('},{')), key=lambda x:len(x))
-
-    for tup in _list:
-        chunk = list(map(int, tup.split(',')))
-        answer.extend(set(chunk)-set(answer))
-    return answer
