ec9f1746e97f2bc939a3f0dfbeab25ca4eb7f4c5
HyunJe Park <sorsor999@naver.com>
2026-01-06 07:44:04 +0900

    add: 구명보트

---
+++ Week11/0106_PGS_구명보트_박현제.py
@@ -0,0 +1,32 @@
+
+"""
+알고리즘
+- 몸무게 큰사람 우선으로 소비 -> 내림차순 정렬
+- 최대한 몸무게 큰사람 + 작은사람 사용
+
+시간복잡도
+- O(50000)
+"""
+def solution(people, limit):
+    answer = 0
+    
+    n = len(people)
+    
+    people.sort(reverse=True)
+    
+    s = 0
+    e = n - 1
+  
+    while s <= e and s < n:
+        # if s == e: # 마지막 사람이기 떄문에 total <= limit 이든 말든 상관없다. 
+        #     answer += 1
+        #     break
+        total = people[s] + people[e] 
+        if total <= limit:
+            s += 1
+            e -= 1
+        else:
+            s += 1
+        answer += 1
+
+    return answer
