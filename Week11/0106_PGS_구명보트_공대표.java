50ed48433c7740976b78ff2197e54da9028dcb50
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-01-06 22:24:06 +0900

    add: 구명보트

---
+++ Week11/0106_PGS_구명보트_공대표.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int result = 0;
+        
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            result++;
+        }
+
+        return result;
+    }
+}
