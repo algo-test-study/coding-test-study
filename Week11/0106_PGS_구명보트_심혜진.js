7df198276a761f2a521b01dc05775e3849eecd78
hyejin <noodle9407@gmail.com>
2026-01-07 23:42:33 +0900

    add: 구명보트

---
+++ Week11/0106_PGS_구명보트_심혜진.js
@@ -0,0 +1,13 @@
+function solution(people, limit) {
+  people.sort((a,b) => a - b);
+
+  let left = 0;
+  let right = people.length-1;
+  let boats = 0;
+
+  while (left <= right) {
+    if (people[left]+people[right] <= limit) {
+      left++;
+    }
+  }
+}
