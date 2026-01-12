64a54a32310001a1179116b2d71a6bbee2dfcd37
HyunJe Park <sorsor999@naver.com>
2026-01-12 23:44:31 +0900

    add: 나무 자르기

---
+++ Week12/0112_BOJ_나무자르기_박현제.py
@@ -0,0 +1,32 @@
+"""
+알고리즘
+- 나무 합 >= m, h 최댓값
+- 절단기 높이 h <= 10^9 -> 최적화: 나무 max값
+- TTTTFFFF -> 파라메트릭 서치
+시간복잡도
+- O(log(h))
+"""n, m = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+s = 0
+e = max(lst)
+
+while s <= e:
+	mid = (s + e)//2
+
+	def is_valid():
+		total = 0
+		for curr in lst:
+			if curr - mid >= 0:
+				total += curr - mid
+		if total >= m:
+			return True
+		return False
+
+	if is_valid():
+		s = mid + 1
+	else:
+		e = mid - 1
+
+print(e)
