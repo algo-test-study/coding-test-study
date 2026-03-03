19d87a22c32fd99c79ee016b691bf8e12540b8e2
023 <rhdgmltkd88@gmail.com>
2026-03-03 21:16:09 +0900

    add: Word Search

---
+++ Week19/0303_LTC_WordSearch_공희상.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)
+                 && dfs(board, i, j, word, 0)) {
+                    return true;
+                 }
+            }
+        }
+
+        return false;
+    }
+
+    boolean dfs(char[][] board, int i, int j, String word, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length 
+         || j < 0 || j >= board[0].length) {
+            return false;
+        }
+
+        if (board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = ' ';
+
+        boolean found = dfs(board, i+1, j, word, index+1)
+                    || dfs(board, i-1, j, word, index+1)
+                    || dfs(board, i, j+1, word, index+1)
+                    || dfs(board, i, j-1, word, index+1);
+
+        board[i][j] = temp;
+
+        return found;
+    }
+}
