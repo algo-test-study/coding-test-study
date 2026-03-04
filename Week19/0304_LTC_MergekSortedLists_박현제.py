54e6008e56d1a45f1c3e2ed0d7c4e60cd45ddc51
HyunJe Park <sorsor999@naver.com>
2026-03-04 23:53:18 +0900

    add: Merge k Sorted Lists

---
+++ Week19/0304_LTC_MergekSortedLists_박현제.py
@@ -0,0 +1,37 @@
+"""
+알고리즘
+- 우선순위 큐
+시간복잡도
+- O(nlogk)
+
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+import heapq
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = []
+
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        temp = ListNode(0)
+        tail = temp
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            tail.next = node
+            tail = tail.next
+
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        tail.next = None
+      
+        return temp.next
+        
