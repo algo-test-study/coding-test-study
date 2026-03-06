e216b27b6a6f24c63cbdad86af9579c9e89ae7ee
심혜진 <82854752+shj78@users.noreply.github.com>
2026-03-06 23:19:01 +0900

    add: Balanced Binary Tree

---
+++ Week19/0306_LTC_BalancedBinaryTree_심혜진.js
@@ -0,0 +1,17 @@
+var isBalanced = function(root) {
+  function dfs(node) {
+    if (!node) return 0;
+
+    const left = dfs(node.left);
+    if (left === -1) return -1;
+
+    const right = dfs(node.right);
+    if (right === -1) return -1;
+
+    if (Math.abs(left - right) > 1) return -1;
+
+    return Math.max(left, right) + 1;
+  }
+
+  return dfs(root) !== -1;
+};
