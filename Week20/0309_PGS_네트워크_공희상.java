675572bb0c0848365e0b7df206686b9ea494549d
023 <rhdgmltkd88@gmail.com>
2026-03-09 20:07:16 +0900

    add: 네트워크

---
+++ Week20/0309_PGS_네트워크_공희상.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(computers, visited, i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    void dfs(int[][] computers, boolean[] visited, int current) {
+        visited[current] = true;
+        
+        for (int i = 0; i < computers.length; i++) {
+            if (current != i && 
+                computers[current][i] == 1 && 
+                !visited[i]) {
+                dfs(computers, visited, i);
+            }
+        }
+        
+    }
+}
