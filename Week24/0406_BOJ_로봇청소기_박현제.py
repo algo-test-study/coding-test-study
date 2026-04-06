1ff47f75014f140848665c78ffc0b67cea284cad
HyunJe Park <sorsor999@naver.com>
2026-04-06 20:32:31 +0900

    fix: 코드 개선

--- Week24/0406_BOJ_로봇청소기_박현제.py
+++ Week24/0406_BOJ_로봇청소기_박현제.py
@@ -1,7 +1,6 @@
 """
 시뮬, O(nm)
 """
-
 import sys
 from collections import deque
 
@@ -9,18 +8,18 @@ from collections import deque
 input = sys.stdin.readline
 ans = 0
 n, m = map(int, input().split())
-sx, sy, d = map(int, input().split())
+cx, cy, d = map(int, input().split())
 
 arr = [list(map(int, input().split())) for i in range(n)]
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
-def isOutOfBound(nx, ny):
+def is_out_of_bounds(nx, ny):
 	return 0 <= nx <= n and 0 <= ny <= m
 
-def dfs(cx, cy):
-	global ans, d
+
+while True:
 
 	if arr[cx][cy] == 0:
 		arr[cx][cy] = -1
@@ -30,24 +29,22 @@ def dfs(cx, cy):
 	
 	for i in range(4):
 		nx, ny = cx + dx[i], cy + dy[i]
-		if isOutOfBound(nx, ny) and arr[nx][ny] == 0:
+		if is_out_of_bounds(nx, ny) and arr[nx][ny] == 0:
 			is_move = True
 
 	if is_move:
 		d = (d + 3)%4
 		nx, ny = cx + dx[d], cy + dy[d]
-		if isOutOfBound(nx, ny) and arr[nx][ny] == 0:
-			dfs(nx, ny)
-			return
-		dfs(cx, cy)
+		if is_out_of_bounds(nx, ny) and arr[nx][ny] == 0:
+			cx, cy = nx, ny
 				
 	else:
 		back = (d + 2)%4
 		nx, ny = cx + dx[back], cy + dy[back]
-		if isOutOfBound(nx, ny) and arr[nx][ny] != 1:
-			dfs(nx, ny)
-
-dfs(sx, sy)
+		if is_out_of_bounds(nx, ny) and arr[nx][ny] != 1:
+			cx, cy = nx, ny
+		else:
+			break
 
 
 print(ans)
