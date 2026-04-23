38db4bf3790959fc6f12d38f6d6c8c9172e5215a
023 <rhdgmltkd88@gmail.com>
2026-04-23 22:03:21 +0900

    add: 구명보트

---
+++ Week26/0423_PGS_구명보트_공희상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int a = 0;
+        int b = people.length - 1;
+        
+        while (a < b) {
+            if (people[a] + people[b] <= limit) {
+                answer++;
+                a++;
+            }
+            b--;
+        }
+        return people.length - answer;
+    }
+}
