fc9028fffeb906effe5a28fd0b21b30d24306298
023 <rhdgmltkd88@gmail.com>
2026-04-27 23:21:09 +0900

    add: 징검다리 건너기

---
+++ Week27/0427_PGS_징검다리건너기_공희상.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(int[] stones, int k) {
+        int left = 1;
+        int right = 0;
+        int answer = 0;
+        
+        for (int stone : stones) {
+            right = Math.max(right, stone);
+        }
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (canOver(stones, mid, k)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+    
+    boolean canOver(int[] stones, int mid, int stepLimit) {
+        int step = 0;
+        
+        for (int stone : stones) {
+            if (stone >= mid) {
+                step = 0;
+            } else {
+                step++;
+                if (step >= stepLimit) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
