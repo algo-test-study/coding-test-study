c031c6851519494a71306b7dad0e4d0ec43372a4
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-04-27 22:30:47 +0900

    add: 징검다리 건너기

---
+++ Week27/0427_PGS_징검다리건너기_공대표.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+
+        int left = 1;
+        int right = 200000000;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (canCross(stones, k, mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    boolean canCross(int[] stones, int k, int mid) {
+        int count = 0;
+
+        for (int stone : stones) {
+            if (stone - mid < 0) {
+                count++;
+                if (count >= k) return false;
+            } else {
+                count = 0;
+            }
+        }
+
+        return true;
+    }
+}
