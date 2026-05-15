d3bd64f58efc7b3cd95304c884a287988243350a
HyunJe Park <sorsor999@naver.com>
2026-05-15 18:16:09 +0900

    add: 방문 길이

---
+++ Week29/0515_PGS_방문길이_박현제.py
@@ -0,0 +1,26 @@
+# 구현, O(len(dirs))
+def solution(dirs):
+    answer = 0
+    n = 5
+    d = {"U": [-1, 0], "D": [1, 0], "L" : [0, -1], "R" : [0, 1]}
+    
+    cx, cy = 0, 0
+    
+    visited = set()
+    
+    cnt = 1
+    
+    for i in dirs:
+        dx, dy = d[i]
+
+        nx, ny = cx + dx, cy + dy
+
+        if -n <= nx <= n and -n <= ny <= n:            
+            visited.add((cx, cy, nx, ny))
+            visited.add((nx, ny, cx, cy))
+            
+            cx, cy = nx, ny
+    
+    answer = len(visited)//2
+    
+    return answer
