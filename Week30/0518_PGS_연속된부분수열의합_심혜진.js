73ac53719b9dca7784df2ba46e89dc4e9ea49464
hyejin <noodle9407@gmail.com>
2026-05-18 23:20:47 +0900

    add: 연속된 부분 수열의 합

---
+++ Week30/0518_PGS_연속된부분수열의합_심혜진.js
@@ -0,0 +1,26 @@
+function solution(sequence, k) {
+  let left = 0;
+  let right = 0;
+  let sum = sequence[0];
+
+  let bestStart = 0;
+  let bestEnd = sequence.length - 1;
+  let bestLen = Infinity;
+
+  while (left <= right && right < sequence.length) {
+    if (sum === k) {
+      const len = right - left + 1;
+      if (len < bestLen) {
+        bestLen = len;
+        bestStart = left;
+        bestEnd = right;
+      }
+
+      sum -= sequence[left];
+      left += 1;
+    } else if (sum < k) {
+      right += 1;
+      if (right < sequence.length) {
+        sum += sequence[right];
+      }
+    } else
