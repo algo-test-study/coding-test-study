22062bfac69a298f7f2d427f80baf2ff82b0a61f
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-05-20 23:43:30 +0900

    add: 정수 삼각형

---
+++ Week30/0520_PGS_정수삼각형_공대표.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i - 1][j];
+                } else if (j == triangle[i].length - 1) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                } else {
+                    triangle[i][j] += Math.max(
+                        triangle[i - 1][j - 1],
+                        triangle[i - 1][j]
+                    );
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int value : triangle[n - 1]) {
+            answer = Math.max(answer, value);
+        }
+
+        return answer;
+    }
+}
