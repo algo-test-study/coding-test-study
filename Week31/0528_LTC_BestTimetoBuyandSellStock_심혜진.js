6d321e17927c9bcbd64b5e60301a0d020912c27e
hyejin <noodle9407@gmail.com>
2026-05-28 23:58:21 +0900

    add: Best Time to Buy and Sell Stock

---
+++ Week31/0528_LTC_BestTimetoBuyandSellStock_심혜진.js
@@ -0,0 +1,17 @@
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i=1; i<prices.length; i++) {
+        if (prices[i]<minPrice) {
+            minPrice = prices[i];
+        } else {
+            const profit = prices[i]-minPrice;
+            if (profit>maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    return maxProfit;
+};
