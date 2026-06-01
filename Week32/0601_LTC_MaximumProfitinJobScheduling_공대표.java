5bb826f1ee00ed6dd7a792c15dc7ba4ab607aa93
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-06-01 23:03:40 +0900

    add: Maximum Profit in Job Scheduling

---
+++ Week32/0601_LTC_MaximumProfitinJobScheduling_공대표.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
+        }
+
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+
+        TreeMap<Integer, Integer> dp = new TreeMap<>(); 
+        dp.put(0, 0);
+        
+        for (int[] job : jobs) {
+            int curProfit = dp.floorEntry(job[0]).getValue() + job[2];
+
+            if (curProfit > dp.lastEntry().getValue()) {
+                dp.put(job[1], curProfit);
+            }
+        }
+
+        return dp.lastEntry().getValue();
+    }
+}
