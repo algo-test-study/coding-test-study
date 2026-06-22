d77b2789aef524ea2251ecb37e255b00ff46c26c
HyunJe Park <sorsor999@naver.com>
2026-06-22 19:05:24 +0900

    add: Unique Paths

---
+++ Week35/0616_LTC_UniquePaths_박현제.py
@@ -0,0 +1,24 @@
+"""
+dp
+O(mn)
+"""
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [ n * [0] for _ in range(m)]
+
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j]
+        
+
+        return dp[m-1][n-1]
