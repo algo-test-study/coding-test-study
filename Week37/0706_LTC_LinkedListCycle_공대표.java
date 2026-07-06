d7527d7252bcbb718448b8adb83b3eee9d7b8bfa
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-07-06 20:33:28 +0900

    add: Linked List Cycle

---
+++ Week37/0706_LTC_LinkedListCycle_공대표.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+
+            set.add(head);
+            head = head.next;
+        }
+
+        return false;
+    }
+}
