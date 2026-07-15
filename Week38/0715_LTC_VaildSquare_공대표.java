24cff4b1bf055f4911bf9f810c88c7b8831742e4
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-07-15 22:47:51 +0900

    add: Vaild Square

---
+++ Week38/0715_LTC_VaildSquare_공대표.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(dist(p1, p2));
+        set.add(dist(p1, p3));
+        set.add(dist(p1, p4));
+        set.add(dist(p2, p3));
+        set.add(dist(p2, p4));
+        set.add(dist(p3, p4));
+
+        return !set.contains(0) && set.size() == 2;
+    }
+
+    private int dist(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+        return dx * dx + dy * dy;
+    }
+}
