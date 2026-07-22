3d1df83e0629fa3c9ff117bc23ff8e588b42333d
023-dev <rhdgmltkd88@gmail.com>
2026-07-22 23:06:04 +0900

    add: 베스트앨범

---
+++ Week39/0722_PGS_베스트앨범_공희상.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    static private class Song {
+        private int id;
+        private int play;
+
+        public Song(int id, int play) {
+            this.id = id;
+            this.play = play;
+        }
+    }
+    public int[] solution(String[] genres, int[] plays) {
+        Map<String, Integer> genrePlayCount = new HashMap<>();
+        Map<String, List<Song>> genreSongs = new HashMap<>();
+
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int play = plays[i];
+            genrePlayCount.put(genre, genrePlayCount.getOrDefault(genre, 0) + play);
+            genreSongs.putIfAbsent(genre, new ArrayList<>());
+            genreSongs.get(genre).add(new Song(i, play));
+        }
+
+        List<String> genreOrder = new ArrayList<>(genrePlayCount.keySet());
+        genreOrder.sort((a, b) -> genrePlayCount.get(b) - genrePlayCount.get(a));
+
+        List<Integer> result = new ArrayList<>();
+        for (String genre : genreOrder) {
+            List<Song> songs = genreSongs.get(genre);
+
+            songs.sort((a, b) -> {
+                if (b.play == a.play) {
+                    return  a.id - b.id;
+                }
+                return b.play - a.play;
+            });
+
+            result.add(songs.get(0).id);
+            if (songs.size() > 1) {
+                result.add(songs.get(1).id);
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        };
+        return answer;
+    }
+}
