52048ea148c76076cf9c9a0341df262499f38a2c
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-08-03 23:38:42 +0900

    add: Backspace String Compare

---
+++ Week41/0803_LTC_BackspaceStringCompare_공대표.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        int sIndex = s.length() - 1;
+        int tIndex = t.length() - 1;
+
+        while (sIndex >= 0 || tIndex >= 0) {
+            int sSkip = 0;
+
+            while (sIndex >= 0) {
+                if (s.charAt(sIndex) == '#') {
+                    sSkip++;
+                    sIndex--;
+                } else if (sSkip > 0) {
+                    sSkip--;
+                    sIndex--;
+                } else {
+                    break;
+                }
+            }
+
+            int tSkip = 0;
+
+            while (tIndex >= 0) {
+                if (t.charAt(tIndex) == '#') {
+                    tSkip++;
+                    tIndex--;
+                } else if (tSkip > 0) {
+                    tSkip--;
+                    tIndex--;
+                } else {
+                    break;
+                }
+            }
+
+            if (sIndex < 0 && tIndex < 0) {
+                return true;
+            }
+
+            if (sIndex < 0 || tIndex < 0) {
+                return false;
+            }
+
+            if (s.charAt(sIndex) != t.charAt(tIndex)) {
+                return false;
+            }
+
+            sIndex--;
+            tIndex--;
+        }
+
+        return true;
+    }
+}
