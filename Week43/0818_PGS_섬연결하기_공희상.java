857d62215fe5710a47906e7744338889d1fc7546
023 <rhdgmltkd88@gmail.com>
2026-08-18 22:33:28 +0900

    add: 섬 연결하기

---
+++ Week43/0818_PGS_섬연결하기_공희상.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    static int[] parent;
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
+        parent = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+        
+        int answer = 0;   
+        int edgeCount = 0;
+        
+        for (int[] cost : costs) {
+            int islandA = cost[0];
+            int islandB = cost[1];
+            int bridgeCost = cost[2];
+            
+            if (find(islandA) != find(islandB)) {
+                union(islandA, islandB);
+                
+                answer += bridgeCost;
+                edgeCount++;
+                
+                if (edgeCount == (n - 1)) {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    private int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    private void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        
+        if (rootA != rootB) parent[rootB] = rootA;
+    }
+}
