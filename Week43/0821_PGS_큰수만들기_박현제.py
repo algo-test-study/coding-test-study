0295087c21cde847bc57b4d95b5364a0fece4a79
HyunJe Park <sorsor999@naver.com>
2026-08-21 23:01:20 +0900

    add: 큰 수 만들기

---
+++ Week43/0821_PGS_큰수만들기_박현제.py
@@ -0,0 +1,23 @@
+"""
+스택, 그리디
+top < curr 일 때 pop
+
+O(n)
+"""
+def solution(number, k):
+    answer = ''
+
+    n = len(number)
+    stk = []
+    cnt = 0
+    
+    for curr in number:
+        
+        while stk and stk[-1] < curr and cnt < k:
+            stk.pop()
+            cnt += 1
+
+        stk.append(curr)
+
+    
+    return "".join(stk[0:n-k])
