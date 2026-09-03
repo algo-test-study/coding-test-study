5d8a024264999fa0e50503cb07cc81c121c33811
HyunJe Park <sorsor999@naver.com>
2026-09-03 23:55:36 +0900

    add: 택배 배달과 수거하기

---
+++ Week45/0903_PGS_택배배달과수거하기_박현제.py
@@ -0,0 +1,18 @@
+"""
+O(n)
+"""
+def solution(cap, n, deliveries, pickups):
+    ans = 0
+    deliver = 0
+    pickup = 0
+
+    for i in range(n - 1, -1, -1):
+        deliver += deliveries[i]
+        pickup += pickups[i]
+
+        while deliver > 0 or pickup > 0:
+            deliver -= cap
+            pickup -= cap
+            ans += (i + 1) * 2
+
+    return ans
