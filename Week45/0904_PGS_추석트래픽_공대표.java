00be95e07f904998da100c45d1c80a00e8b55f6c
대표랑 <79506471+dpyorang@users.noreply.github.com>
2026-09-04 23:16:09 +0900

    add: 추석 트래픽

---
+++ Week45/0904_PGS_추석트래픽_공대표.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] lines) {
+        int n = lines.length;
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            String[] s = lines[i].split(" ");
+            String[] time = s[1].split(":");
+
+            int h = Integer.parseInt(time[0]);
+            int m = Integer.parseInt(time[1]);
+            int sec = Integer.parseInt(time[2].substring(0, 2));
+            int ms = Integer.parseInt(time[2].substring(3));
+
+            end[i] = (h * 3600 + m * 60 + sec) * 1000 + ms;
+
+            String t = s[2].replace("s", "");
+            double d = Double.parseDouble(t);
+            int duration = (int) (d * 1000);
+
+            start[i] = end[i] - duration + 1;
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            int windowStart = end[i];
+            int windowEnd = windowStart + 999;
+
+            for (int j = 0; j < n; j++) {
+                if (start[j] <= windowEnd && end[j] >= windowStart) {
+                    count++;
+                }
+            }
+
+            answer = Math.max(answer, count);
+        }
+
+        return answer;
+    }
+}
