00d37999ac538d0f3eb07f5df00853c8c6fafd01
hyejin <noodle9407@gmail.com>
2026-09-08 17:12:49 +0900

    update: 크레인 인형뽑기 게임 재풀이

--- Week44/0824_PGS_크레인인형뽑기게임_심혜진.js
+++ Week44/0824_PGS_크레인인형뽑기게임_심혜진.js
@@ -24,3 +24,46 @@ function solution(board, moves) {
 
     return answer;
 }
+
+/*
+//시간복잡도 계산
+// moves 배열 최대 1000, board 배열 행 개수 최대 30
+// for문 n이라고 했을 때 내부 while 문으로 처리하는 루프가 최대 30번으로 상수 취급해도 되는 값이 되어 O(n)
+function solution(board, moves) {
+    let answer = 0;
+    
+    let stack = [];
+
+    //크레인 열
+    for(let i=0; i<moves.length; i++){
+        //게임 화면 행을 표시할 변수
+        let crainHgt = 0;
+        
+        //크레인 행
+        while(crainHgt < board.length && board[crainHgt][moves[i]-1]==0){
+            crainHgt++;
+        }
+        if (crainHgt === board.length) continue;
+        
+        //행이 확정된 후 변수화
+        const item = board[crainHgt][moves[i]-1];
+        
+        //집을 인형이 없을 경우
+        if(item === 0) continue;
+        
+        //집은 인형 제거
+        board[crainHgt][moves[i]-1]=0;
+
+        
+        //행이 확정되고 집은 인형을 바구니로
+        if(stack[stack.length-1] === item){
+            answer+=2;
+            stack.pop();
+        }else{
+            stack.push(item);                    
+        }
+    }
+    
+    return answer;
+}
+*/
