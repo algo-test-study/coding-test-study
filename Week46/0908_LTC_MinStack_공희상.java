45c071e2771203feddc14064ec5b57c0c68c53eb
023 <rhdgmltkd88@gmail.com>
2026-09-08 23:47:17 +0900

    add: Min Stack

---
+++ Week46/0908_LTC_MinStack_공희상.java
@@ -0,0 +1,30 @@
+class MinStack {
+
+    private final Stack<Integer> stack = new Stack<>();
+    private final Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
