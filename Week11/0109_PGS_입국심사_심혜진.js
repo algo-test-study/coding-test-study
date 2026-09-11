eb01d5208239513b4d30363570fecbe1243683ec
hyejin <noodle9407@gmail.com>
2026-09-11 23:36:34 +0900

    add: Sliding Window Maximum

--- Week11/0109_PGS_입국심사_심혜진.js
+++ Week11/0109_PGS_입국심사_심혜진.js
@@ -22,3 +22,34 @@ function solution(n, times) {
 
   return answer;
 }
+/*
+// n과 times의 값 범위 각각 10^9, 10^9, 
+// 모든 사람이 심사받는 최소 시간
+// 답이 되는 후보 값의 범위가 10^18이 될 수 있음
+// 브루트 포스 불가로 범위를 줄여야함 > 이분 탐색
+
+// 시간 복잡도 O(logN*M)
+
+function solution(n, times) {
+    let start = 1;
+    let end = Math.max(...times)*n;
+    let answer = end;
+    
+    while(start<=end){
+        const mid = Math.floor((start+end)/2);
+        let count = 0;
+        for(let i=0; i<times.length; i++){
+            count  += Math.floor(mid/times[i]);
+            if (count >= n) break;
+        }
+        
+        if(count>=n){
+            answer = mid;
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }     
+    }
+  return answer;
+}
+*/
