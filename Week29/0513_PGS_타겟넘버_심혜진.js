2d204dd07688fbd31ef73058ee0613dcd89de166
hyejin <noodle9407@gmail.com>
2026-09-11 12:05:35 +0900

    update: 타겟넘버 재풀이

--- Week29/0513_PGS_타겟넘버_심혜진.js
+++ Week29/0513_PGS_타겟넘버_심혜진.js
@@ -1,19 +1,40 @@
 function solution(numbers, target) {
   let answer = 0;
 
-  function dfs(candidateTarget, currentIndex){
-      if(currentIndex === numbers.length){
-        if(candidateTarget === target){
-          answer++;
-        }
-        return;
+  function dfs(candidateTarget, currentIndex) {
+    if (currentIndex === numbers.length) {
+      if (candidateTarget === target) {
+        answer++;
       }
+      return;
+    }
 
-      dfs(candidateTarget+numbers[currentIndex], currentIndex+1);
-      dfs(candidateTarget-numbers[currentIndex], currentIndex+1);
+    dfs(candidateTarget + numbers[currentIndex], currentIndex + 1);
+    dfs(candidateTarget - numbers[currentIndex], currentIndex + 1);
 
   }
 
   dfs(0, 0);
   return answer;
 }
+/*
+//완전탐색 DFS
+function solution(numbers, target) {
+  let answer = 0;
+    
+  function dfs(currentIndex, val){
+      if(currentIndex === numbers.length){
+          if(val === target) answer++;
+          return;
+      }
+      
+      //두 갈래로 이진트리를 DFS로 돌기때문에 시간복잡도 O(2^n)
+      dfs(currentIndex+1, val+numbers[currentIndex])
+      dfs(currentIndex+1, val+-numbers[currentIndex]) 
+  }
+
+  dfs(0,0);
+  return answer;
+}
+
+*/
